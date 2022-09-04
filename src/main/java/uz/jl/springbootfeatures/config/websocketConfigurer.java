e66c2b1bd445d262af755e0025bbf3f639ac9d71
ShohimardonDev03 <q@u2p@3jW5Brw5c>
2022-09-04 22:43:23 +0500

    lost commit

---
+++ src/main/java/uz/jl/springbootfeatures/config/websocketConfigurer.java
@@ -0,0 +1,11 @@
+package uz.jl.springbootfeatures.config;
+
+/**
+ * @author: Shohimardon Abdurashitov
+ * @since; September  Sunday 22:27:44
+ * @product Name:  spring-boot-161
+ * @Class Name: websocketConfic
+ * IntelliJ IDEA
+ **/
+public class websocketConfigurer {
+}
